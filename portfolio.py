51ffcd8d948ecd6fa5b90af6ce818592a4cb50a3
RokhayaFall1 <fallrokhaya146@gmail.com>
2026-02-27 17:27:16 +0100

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -4,7 +4,7 @@ st.title("Rokhaya Fall")
 st.header("Géomaticienne")
 with st.sidebar:
     #1.information personnelles
-    st.image("img.jpg")
+    st.image("fall.jpg")
     st.title("Rokhaya FALL")
     st.markdown("Adresse: Keur Mbaye Fall, Dakar")
     st.markdown("Email: fallrokhaya146@gmail.com")
